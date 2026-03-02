a6d577cc4f0d5e1165d4d54b3a92cb87064a09fd
Aaron Jackson <AJackson002@dundee.ac.uk>
2026-03-02 14:56:18 +0000

    Make rDNS less disgusting (4)

--- agnostics/ad/k3s/base/samba-stop.sh
+++ agnostics/ad/k3s/base/samba-stop.sh
@@ -10,4 +10,4 @@ export KRB5CCNAME=/ccache
 echo "Removing rDNS record"
 MYIP=$(ip a | grep -A1 link/ether | grep inet | awk '{ print $2 }' | awk -F'/' '{ print $1 }')
 MYRIP=$(echo $MYIP | awk -F. '{ print $4"."$3"."$2 }')
-samba-tool dns delete dc0 10.in-addr.arpa $MYRIP PTR dc0.$REALM. --use-krb5-ccache=/ccache
+samba-tool dns delete dc0 10.in-addr.arpa $MYRIP PTR dc0.$REALM. --use-krb5-ccache=/ccache || true
